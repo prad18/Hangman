04bf85caab31c74ff2dea3cc59416d5a40e528b2
pradish <pradishvaidyas@gmail.com>
2023-12-28 22:34:34 +0530

    commit

---
+++ scripts/script.js
@@ -0,0 +1,98 @@
+const wordDisplay = document.querySelector(".word-display");
+const guessesText = document.querySelector(".guesses-text b");
+const keyboardDiv = document.querySelector(".keyboard");
+const hangmanImage = document.querySelector(".hangman-box img");
+const gameModal = document.querySelector(".game-modal");
+const playAgainBtn = gameModal.querySelector("button");
+
+// Initializing game variables
+let currentWord, correctLetters, wrongGuessCount, wordIndex;
+const maxGuesses = 6;
+
+const resetGame = () => {
+    // Resetting game variables and UI elements
+    correctLetters = [];
+    wrongGuessCount = 0;
+    hangmanImage.src = "./images/hangman-0.svg";
+    guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
+    wordDisplay.innerHTML = currentWord.split("").map(() => `<li class="letter"></li>`).join("");
+    keyboardDiv.querySelectorAll("button").forEach(btn => btn.disabled = false);
+    gameModal.classList.remove("show");
+}
+
+const getRandomWord = () => {
+    // Selecting a word and hint from the wordList in order
+    const { word, hint } = wordList[wordIndex];
+
+    // Increment the index for the next round
+    wordIndex = (wordIndex + 1) % wordList.length;
+
+    currentWord = word; // Making currentWord as the selected word
+    document.querySelector(".hint-text b").innerText = hint;
+    resetGame();
+}
+
+const gameOver = (isVictory) => {
+    // After the game is complete, show the modal with relevant details
+    const modalText = isVictory ? `You found the word:` : 'The correct word was:';
+    gameModal.querySelector("img").src = `images/${isVictory ? 'victory' : 'lost'}.gif`;
+    gameModal.querySelector("h4").innerText = isVictory ? 'Congrats!' : 'Game Over!';
+    gameModal.querySelector("p").innerHTML = `${modalText} <b>${currentWord}</b>`;
+
+    // Change the text of the play again button based on the result
+    playAgainBtn.innerText = isVictory ? 'Next Word' : 'Try Again';
+
+    // If all words are used, change the button text to something else
+    if (wordIndex === 0) {
+        playAgainBtn.innerText = 'Reload page to play again';
+        playAgainBtn.disabled = true; // Optionally, disable the button
+    }
+
+    gameModal.classList.add("show");
+}
+
+const initGame = (button, clickedLetter) => {
+    // Checking if clickedLetter exists in the currentWord (case-insensitive)
+    if (currentWord.toLowerCase().includes(clickedLetter.toLowerCase())) {
+        // Showing all correct letters on the word display
+        [...currentWord].forEach((letter, index) => {
+            if (letter.toLowerCase() === clickedLetter.toLowerCase()) {
+                correctLetters.push(letter);
+                wordDisplay.querySelectorAll("li")[index].innerText = letter;
+                wordDisplay.querySelectorAll("li")[index].classList.add("guessed");
+            }
+        });
+    } else {
+        // If clicked letter doesn't exist, update the wrongGuessCount and hangman image
+        wrongGuessCount++;
+        hangmanImage.src = `images/hangman-${wrongGuessCount}.svg`;
+    }
+
+    button.disabled = true; // Disabling the clicked button so the user can't click again
+    guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
+
+    // Calling gameOver function if any of these conditions are met
+    if (wrongGuessCount === maxGuesses) return gameOver(false);
+
+    // Checking if all letters in the word have been guessed
+    if (currentWord.split("").every(letter => correctLetters.includes(letter.toLowerCase()))) {
+        return gameOver(true);
+    }
+};
+
+// Creating keyboard buttons and adding event listeners
+for (let i = 97; i <= 122; i++) {
+    const button = document.createElement("button");
+    button.innerText = String.fromCharCode(i);
+    keyboardDiv.appendChild(button);
+    button.addEventListener("click", (e) => initGame(e.target, String.fromCharCode(i)));
+}
+
+// Initialize wordIndex
+wordIndex = 0;
+
+// Start the first game
+getRandomWord();
+
+playAgainBtn.addEventListener("click", getRandomWord);
+
---
+++ scripts/word-list.js
@@ -0,0 +1,46 @@
+const wordList = [
+    {
+        word: "voltage",
+        hint: "1)Traditional computing is sensitive to variation in ____ levels which has cause errors "
+    },
+    {
+        word: "loihi",
+        hint: "2)Example of neuromorphic computres intel"
+    },
+    {
+        word: "spikes",
+        hint: "3)What does neural networks use to communicate between neurons?"
+    },
+    {
+        word: "truenorth",
+        hint: "4)Example of neuromorphic computers ibm?"
+    },
+    {
+        word: "noise",
+        hint: "5) Traditional computing is sensitive to ______"
+    },
+    {
+        word: "electrochemical",
+        hint: "6) Neurons transmits signals through ______ pulses"
+    },
+    {
+        word: "neuron",
+        hint: "7)What is the fundamental building block of an Artificial Neural Network (ANN)?"
+    },
+    {
+        word: "carver",
+        hint: "8) Neuromorphic computing was first proposed by _____ mead"
+    },
+    {
+        word: "cognitive",
+        hint: "9) _____ computing is an application of nueromorphic computing"
+    },
+    {
+        word: "parallel",
+        hint: "10)____ processing is a key feature of neuromorphic computing"
+    },
+    {
+        word: "brain",
+        hint: "11)Neuromorphic computing mimics _____'s architecture"
+    }
+];
